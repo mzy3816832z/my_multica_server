520cf1875c162fc7d2722f81336d08b57bc4b2b2
mzy3816832z <806007035@qq.com>
2026-07-23 11:18:41 +0800

    增加csrf

--- config/settings.py
+++ config/settings.py
@@ -172,11 +172,7 @@ CORS_ALLOWED_ORIGINS = [
     if origin.strip()
 ]
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = [
-    origin.strip()
-    for origin in os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5174').split(',')
-    if origin.strip()
-]
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 
 # drf-spectacular
 SPECTACULAR_SETTINGS = {
